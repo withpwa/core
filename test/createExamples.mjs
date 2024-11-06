3e6ab1263d2fa61cebd303ff0ab6334c62f7310a
Jaki Chen <JakiChen@live.cn>
2024-11-06 18:09:46 +0800

    Update createExamples.mjs

--- test/createExamples.mjs
+++ test/createExamples.mjs
@@ -1,7 +1,7 @@
 /**
-    Make sure you have the lates build. Run `npm run build` to rebuild.
-    Run `node createExamples.js` to create the icons, splashs and files.
-**/
+ * Make sure you have the lates build. Run `npm run build` to rebuild.
+ * Run `node createExamples.js` to create the icons, splashs and files.
+ */
 
 import { compose } from "../dist/index.mjs";
 import fs from "fs/promises";
@@ -10,15 +10,13 @@ import { fileURLToPath } from "url";
 const source = fileURLToPath(new URL("fixtures/logo.png", import.meta.url)); // Source image(s). `string`, `buffer` or array of `string`
 
 const configuration = {
-  output: {
-    assetsPrefex: "/",
-  }, // Path for overriding default icons path. `string`
   name: null, // Your application's name. `string`
+  thmes: ["#fff", "#fff"], // `theme-color` support user's prefers color scheme.
+  background: "#fff", // Background colour for flattened icons. `string`
   manifest: {
     short_name: null,
     dir: "auto", // Primary text direction for name, short_name, and description
     lang: "en-US", // Primary language for name and short_name
-    background: "#fff", // Background colour for flattened icons. `string`
     theme_color: "#fff", // Theme color user for example in Android's task switcher. `string`
     appleStatusBarStyle: "black-translucent", // Style for Apple status bar: "black-translucent", "default", "black". `string`
     display: "standalone", // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
@@ -27,7 +25,7 @@ const configuration = {
     start_url: "/?homescreen=1", // Start URL when launching the application from a device. `string`
     prefer_related_applications: false, // Should the browser prompt the user to install the native companion app. `boolean`
     related_applications: undefined, // Information about the native companion apps. This will only be used if `preferRelatedApplications` is `true`. `Array<{ id: string, url: string, platform: string }>`
-    // Other manifest keys now supported .....
+    // Other manifest supported by w3org
   },
   cacheBustingQueryParam: null, // Query parameter added to all URLs that acts as a cache busting system. `string | null`
   version: "1.0", // Your application's version string. `string`
@@ -40,6 +38,14 @@ const configuration = {
     favicons: true, // Create regular compose. `boolean` or `{ offset, background }`
     windowsTile: true, // Create Windows 8 tile icons. `boolean` or `{ offset, background }`
     yandex: true, // Create Yandex browser icon. `boolean` or `{ offset, background }`
+  },
+  shortcuts: [],
+  screenshots: [],
+  output: {
+    images: true,
+    files: true,
+    html: true,
+    assetsPrefex: "/",
   },
 };
 
